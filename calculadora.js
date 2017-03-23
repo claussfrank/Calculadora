2182c50d132abcb87775e8039cf0244379b78421
Clauss Franco <Clauss Franco>
2017-03-23 17:18:16 -0600

    Primera edicion

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+function boton(num){
+    var numero = document.calculadora.resultado.value;
+    document.calculadora.resultado.value= numero+ num;
+}
+function sumarNumeros(n1,n2){
+            return(parseInt(n1)+parseInt(n2));
+        }
+function restarNumero(n1,n2){
+            return(parseInt(n1)-parseInt(n2));
+        }
+function multiplicarNumero(n1,n2){
+            return(parseInt(n1)*parseInt(n2));
+        }
+function dividirNumeros(n1,n2){
+            return(parseInt(n1)/parseInt(n2));    
+        }
